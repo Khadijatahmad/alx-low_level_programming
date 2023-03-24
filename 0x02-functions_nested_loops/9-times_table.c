049a094c5300118100f06c5b267e017b1f8a0136
garbau <garbaumar49@gmail.com>
2023-03-24 11:35:26 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,28 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int product 
-		
-		for (i = 0; i <= 9; i++)
+	int product;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
 		{
-			for (j = 0; j <= 9; j++)
+			product = i * j;
+			if (product > 9)
 			{
-				product = i * j;
-				if (product > 9)
-				{
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else
-				{
-					_putchar(product + '0');
-				}
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar(product + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+	}
 }
